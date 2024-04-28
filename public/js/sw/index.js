c137f8134c7d0e2df5785ce76246206262192056
Iyadi Cyril <iyadicy@gmail.com>
2024-04-28 22:04:10 +0100

    extend: caching resources

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,24 @@
+self.addEventListener("install", function(event) {
+  const urlsToCache = [
+    '/',
+    'js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+  ];
+
+  event.waitUntil(
+    caches.open('wittr-static-v1').then(function(cache){
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
 self.addEventListener("fetch", function(event){
-  console.log(event.request);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
