918e0762e2d7be721890388c892cdf2be63c6620
Iyadi Cyril <iyadicy@gmail.com>
2024-05-21 22:18:42 +0100

    refactor: add new stores and populate them.

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,8 +1,19 @@
 import idb from 'idb';
 
 var dbPromise = idb.open('test-db', 1, function(upgradeDb) {
-  var keyValStore = upgradeDb.createObjectStore('keyval');
-  keyValStore.put("world", "hello");
+  switch(upgradeDb.oldVersion) {
+    case 0:
+      var keyValStore = upgradeDb.createObjectStore('keyval');
+      keyValStore.put("world", "hello");
+      break;
+    case 1:
+      upgradeDb.createObjectStore('people', { keyPath: 'name' });
+      break;
+    case 2:
+      var peopleStore = upgradeDb.transaction.objectStore('people');
+      peopleStore.createIndex('animal', 'favoriteAnimal');
+  }
+  // TODO: create an index on 'people' named 'age', ordered by 'age'
 });
 
 // read "hello" in "keyval"
@@ -33,3 +44,50 @@ dbPromise.then(function(db) {
 }).then(function() {
   console.log('Added favoriteAnimal:Red Panda to keyval');
 });
+
+// add people to "people"
+dbPromise.then(function(db) {
+  var tx = db.transaction('people', 'readwrite');
+  var peopleStore = tx.objectStore('people');
+
+  peopleStore.put({
+    name: 'Sam Munoz',
+    age: 25,
+    favoriteAnimal: 'dog'
+  });
+
+  peopleStore.put({
+    name: 'Susan Keller',
+    age: 34,
+    favoriteAnimal: 'cat'
+  });
+
+  peopleStore.put({
+    name: 'Lillie Wolfe',
+    age: 28,
+    favoriteAnimal: 'dog'
+  });
+
+  peopleStore.put({
+    name: 'Marc Stone',
+    age: 39,
+    favoriteAnimal: 'cat'
+  });
+
+  return tx.complete;
+}).then(function() {
+  console.log('People added');
+});
+
+// list all cat people
+dbPromise.then(function(db) {
+  var tx = db.transaction('people');
+  var peopleStore = tx.objectStore('people');
+  var animalIndex = peopleStore.index('animal');
+
+  return animalIndex.getAll('cat');
+}).then(function(people) {
+  console.log('Cat people:', people);
+});
+
+// TODO: console.log all people ordered by age
